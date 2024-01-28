1c8a582cf6cdd315b9eb7861967ea785627ba82a
HclassServer <125135454+HclassServer@users.noreply.github.com>
2024-01-28 13:06:40 +0800

    Update update.js

--- update.js
+++ update.js
@@ -2,8 +2,8 @@ document.getElementById("pBox").innerHTML = "当前已经是最新版本了";
 
 
 function addUpdateButton(){
-	let varButton = document.createElement("button");
 	document.getElementById("pBox").innerHTML = "提取码：tpup<br>如果您已更新至1.1，请不用再次理会(查看关于)";
+	let varButton = document.createElement("button");
 	varButton.id = "updataButton";
 	varButton.innerHTML = "点我更新 <br>;
 	varButton.setAttribute("onclick","window.location.href = 'https://pan.baidu.com/s/1srKojd276ebtlempbfi9Hw?pwd=tpup'")
