f13b6f242672b64ce5ae2b9443fd8ae60e2d0924
HclassServer <125135454+HclassServer@users.noreply.github.com>
2024-01-28 12:33:34 +0800

    add v1.1 update

--- update.js
+++ update.js
@@ -4,7 +4,9 @@ document.getElementById("pBox").innerHTML = "当前已经是最新版本了";
 function addUpdateButton(){
 	let varButton = document.createElement("button");
 	varButton.id = "updataButton";
-	varButton.innerHTML = "点我更新";
-	varButton.setAttribute("onclick","window.location.href = 'https://'")
+	varButton.innerHTML = "点我更新 <br> 如果您已更新至1.1，请不用再次理会(查看关于)";
+	varButton.setAttribute("onclick","window.location.href = 'https://wwz.lanzouj.com/iDzgW1miym6j'")
 	document.getElementById("bottonBox").appendChild(varButton);
 }
+
+addUpdateButton();
