79c4a2dc3ed52690c9661f0f67b92ff439d76670
HclassServer <125135454+HclassServer@users.noreply.github.com>
2024-01-28 13:21:02 +0800

    Update update.js

--- update.js
+++ update.js
@@ -1,8 +1,7 @@
-document.getElementById("pBox").innerHTML = "当前已经是最新版本了";
+document.getElementById("pBox").innerHTML = "提取码：tpup<br>如果您已更新至1.1，请不用再次理会(查看关于)";
 
 
 function addUpdateButton(){
-	document.getElementById("pBox").innerHTML = "提取码：tpup<br>如果您已更新至1.1，请不用再次理会(查看关于)";
 	let varButton = document.createElement("button");
 	varButton.id = "updataButton";
 	varButton.innerHTML = "点我更新 <br>;
